04067b738131b68586fcd0cbbaeece89240500b2
IL <irewlg@outlook.com>
2025-05-23 23:24:17 -0400

    updated

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,21 +1,38 @@
 import {useState} from "react";
+import { Link } from 'react-router';
+import axios from 'axios';
+
 function Signup() {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3001/register', {name, email, password})
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
+
     return (
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100>">
             <div className="bg-white p-3 rounded w-25">
                 <h2>Register</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-3">
-                        <label htmlFor="email">
+                        <label htmlFor="name">
                             <strong>Name</strong>
                         </label>
                         <input
                         type="text"
                         placeholder="Enter Name"
                         autoComplete="off"
-                        name="email"
+                        name="name"
                         className="form-control rounded-0"
+                        onchange = {(e) => setName(e.target.value)}
                         />
+                        <div class="invalid-feedback">
+                            Please enter your name
+                        </div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="email">
@@ -27,10 +44,14 @@ function Signup() {
                           autoComplete="off"
                           name="email"
                           className="form-control rounded-0"
+                          onchange = {(e) => setEmail(e.target.value)}
                         />
+                        <div class="invalid-feedback">
+                            Please enter your email
+                        </div>
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="email">
+                        <label htmlFor="password">
                             <strong>Password</strong>
                         </label>
                         <input 
@@ -38,16 +59,20 @@ function Signup() {
                            placeholder="Enter Password"
                            name="password"
                            className="form-control rounded-0"
+                           onchange = {(e) => setPassword(e.target.value)}
                         />
+                        <div class="invalid-feedback">
+                            Please enter your password
+                        </div>
                     </div>
                     <button type="submit" className ="btn btn-success w-100 rounded-0">
                     Register
                     </button>
-                    <p>Already Have an account</p>
-                    <button className="btn btn-default border w-100 bg-light rounded-0 text-declaration-none">
-                      Login
-                    </button>
                 </form>
+                <p>Already Have an account</p>
+                <Link to="/login" className="btn btn-default border w-100 bg-light rounded-0 text-declaration-none">
+                  Login
+                </Link>
             </div>
         </div>
     )
