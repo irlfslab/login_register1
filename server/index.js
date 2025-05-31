ba7bf1faa8cc54674f8434a1f7012432f1975d36
IL <irewlg@outlook.com>
2025-05-31 00:13:17 -0400

    updated

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://127.0.0.1:27017/logac");
+mongoose.connect("mongodb://127.0.0.1:27017/register");
 
 app.post("/login", (req, res) => {
     const {email, password} = req.body;
